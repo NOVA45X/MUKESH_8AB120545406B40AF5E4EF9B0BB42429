2c10d142216b4a6da0d294aa5b2d5b6af81ca02c
NOVA45X <144228091+NOVA45X@users.noreply.github.com>
2023-10-11 11:35:32 +0530

    Update and rename main (2).py to main (4).py

--- main (4).py
+++ main (4).py
@@ -1,4 +1,4 @@
-def linear_search_product(product_list, target_product):
+xvdef linear_search_product(product_list, target_product):
     indices = []
     for i, product in enumerate(product_list):
         if product == target_product:
